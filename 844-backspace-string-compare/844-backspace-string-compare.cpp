12912358f9992a051fc43ec2ac08840ea9ada5ff
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2022-05-09 22:58:10 +0530

    Time: 5 ms (21.35%), Space: 6.5 MB (21.68%) - LeetHub

--- 844-backspace-string-compare/844-backspace-string-compare.cpp
+++ 844-backspace-string-compare/844-backspace-string-compare.cpp
@@ -7,13 +7,17 @@ public:
         stack<char>s2;
         for(int i=0;i<a;i++)
         {
-            if(s[i]=='#' && !s1.empty())s1.pop();
-            else if(s[i]!='#')s1.push(s[i]);
+            if(s[i]=='#' && !s1.empty())
+                s1.pop();
+            else if(s[i]!='#')
+                s1.push(s[i]);
         } 
         for(int i=0;i<b;i++)
         {
-            if(t[i]=='#' && !s2.empty())s2.pop();
-            else if(t[i]!='#' )s2.push(t[i]);
+            if(t[i]=='#' && !s2.empty())
+                s2.pop();
+            else if(t[i]!='#' )
+                s2.push(t[i]);
         }
         return s1==s2;
     }
