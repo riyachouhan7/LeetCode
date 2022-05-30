0162335e5f073e5ab626e41a2c2b2cde70728ae9
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2022-05-31 03:22:13 +0530

    Time: 3 ms (67.47%), Space: 5.8 MB (96.08%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.cpp
+++ 29-divide-two-integers/29-divide-two-integers.cpp
@@ -1,6 +1,23 @@
 class Solution {
 public:
     int divide(int dividend, int divisor) {
-        return (dividend == INT_MIN && divisor == -1) ? INT_MAX : dividend / divisor;
+        if(dividend==INT_MIN && divisor==-1) return INT_MAX;
+        if(dividend==INT_MIN && divisor==1) return INT_MIN;
+        
+        long long dd = abs(dividend), dv=abs(divisor);
+        
+        int res=0;
+        while(dv<=dd){
+            long int sum=dv, count=1;
+            while(sum<=dd-sum){
+                sum+=sum;
+                count+=count;
+            }
+            res+=count;
+            dd-=sum;
+        }
+        if((dividend<0 && divisor>0) || (dividend>0 && divisor<0))
+            return -res;
+        return res;
     }
 };
