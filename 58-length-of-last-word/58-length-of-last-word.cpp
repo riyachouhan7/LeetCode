229b1feb9f43b09824af1eab13787d0267f1c7e6
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2022-06-02 10:55:20 +0530

    Time: 4 ms (36.06%), Space: 6.5 MB (89.40%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int n=s.size(),count=0;
+        for(int i=n-1;i>=0;i--)
+        {
+            if(s[i]!=' ')
+                count++;
+            else if(count>0)
+                return count;
+        }
+        return count;
+    }
+};
