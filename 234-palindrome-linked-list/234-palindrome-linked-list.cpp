c3011563bf2b7aafe925c9107672dd51050b3940
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2022-06-15 23:39:31 +0530

    Time: 301 ms (56.83%), Space: 128.3 MB (14.17%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/234-palindrome-linked-list.cpp
@@ -14,10 +14,15 @@ public:
         vector<int> nums;
         ListNode* curr = head;
         
-        while(curr != NULL){ nums.push_back(curr->val); curr = curr->next;}
+        while(curr != NULL)
+        { 
+            nums.push_back(curr->val); curr = curr->next;
+        }
         
-        for(int i = 0; i < nums.size() / 2; i++){
-            if(nums[i] != nums[nums.size() - i - 1]) return false;
+        for(int i = 0; i < nums.size() / 2; i++)
+        {
+            if(nums[i] != nums[nums.size() - i - 1]) 
+                return false;
         }
         
         return true;
