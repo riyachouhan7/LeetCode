f50f602a30e9c61b73e611905e58576377e6704c
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2022-07-17 19:00:08 +0530

    Time: 5 ms (50.00%), Space: 9.3 MB (100.00%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -5,7 +5,7 @@ public:
         map<int,int>m;
         int size = nums.size();
         
-        int countPairs=0,leftOvers=0;
+        vector<int> v(2,0);
         for(int i=0; i<size; i++)
         {
             m[nums[i]]++;
@@ -13,9 +13,9 @@ public:
         
         for(auto x : m)
         {
-            countPairs += x.second/2;
-            leftOvers += x.second%2;
+            v[0] += x.second/2;
+            v[1] += x.second%2;
         }
-        return {countPairs,leftOvers};
+        return v;
     }
 };
