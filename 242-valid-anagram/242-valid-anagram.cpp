d90869c3edd47f82e80046f25e4f773b5fc6075d
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2022-07-28 09:37:06 +0530

    Time: 17 ms (44.89%), Space: 7.4 MB (7.79%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -5,7 +5,7 @@ public:
         if (s.size() != t.size()) 
             return false;
         
-        unordered_map<char,int> map1, map2;
+        map<char,int> map1, map2;
         for(int i=0;i<s.size();i++)
         {
             map1[s[i]]++;
