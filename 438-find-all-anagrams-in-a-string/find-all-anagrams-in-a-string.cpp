86d5820923b2785c7f90d2963628b2a46f72922e
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-01 09:41:54 +0530

    Time: 6 ms (58.48%) | Memory: 11.8 MB (31.86%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.cpp
@@ -5,7 +5,7 @@ public:
         //taking freq of element of s1
         for(auto it: p) mp[it]++;
         vector<int> ans;
-        int count = mp.size();
+        int zeroCount = mp.size();
         int i = 0; 
         int j = 0;
         int k = p.size();
@@ -14,17 +14,17 @@ public:
             //doing calculation 
             if(mp.find(s[j]) != mp.end()){
                 mp[s[j]]--;
-                if(mp[s[j]] == 0) count--;
+                if(mp[s[j]] == 0) zeroCount--;
             }
              //when we don't hit the window
             if(j-i+1 < k) j++;
              //when hit the window
             else if(j-i+1 == k){
-                if(count == 0) ans.push_back(i);
+                if(zeroCount == 0) ans.push_back(i);
                 if(mp.find(s[i]) != mp.end()){
                     mp[s[i]]++;
                     if(mp[s[i]] == 1){
-                        count++;
+                        zeroCount++;
                     }
                 }
                 i++;
