dfb1ffab83868ee4987ec24c19c2f4131e3bf141
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-03 08:48:56 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (64.02%) - LeetSync

---
+++ 55-jump-game/jump-game.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) 
+    {
+        int n = nums.size();
+        int maxIndex = 0;
+        for(int i=0;i<n;i++)
+        {   
+            if(maxIndex < i)
+            {
+                return false;
+            }
+            maxIndex = max(maxIndex, i + nums[i]);
+        }
+        return true;
+    }
+};
