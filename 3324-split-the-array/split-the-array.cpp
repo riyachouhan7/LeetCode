0291b2b639ca06108758ef1517bc44d20331710e
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-10 22:32:34 +0530

    Time: 2 ms (40.67%) | Memory: 25 MB (71.35%) - LeetSync

--- 3324-split-the-array/split-the-array.cpp
+++ 3324-split-the-array/split-the-array.cpp
@@ -1,10 +1,11 @@
 class Solution {
 public:
     bool isPossibleToSplit(vector<int>& nums) {
-        vector<int> count(101, 0);
+        vector<int> freq(101, 0);
         for(int num : nums)
         {
-            if(++count[num] > 2) 
+            freq[num]++;
+            if(freq[num] > 2) 
             {
                 return false;
             }
