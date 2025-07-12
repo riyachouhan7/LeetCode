e47786af00738018be406d1f78fe418b69640b1e
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-13 04:34:53 +0530

    Time: 51 ms (64.70%) | Memory: 98.5 MB (50.81%) - LeetSync

---
+++ 2294-minimum-time-to-complete-trips/minimum-time-to-complete-trips.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool solve(vector<int>& time, int total, long long n){
+        long long sum = 0;
+        for(auto i : time) {
+            sum += n / i;
+            if(sum >= total) return true;
+        }
+
+        return false;
+    }
+
+    long long minimumTime(vector<int>& time, int totalTrips) {
+        long long l = 0, r = 1e14;
+        while(l <= r) {
+            long long mid = l + (r - l) / 2;
+            if(solve(time, totalTrips, mid))
+            {
+                r = mid - 1;
+            }
+            else l = mid + 1;
+        }
+        return l;
+    }
+};
