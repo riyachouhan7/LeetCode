cdd9ea8c9b94a7bcc6424b6bdc8bf8b3ecc217f5
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-15 09:25:53 +0530

    Time: 424 ms (14.67%) | Memory: 291.7 MB (12.46%) - LeetSync

--- 5-longest-palindromic-substring/longest-palindromic-substring.cpp
+++ 5-longest-palindromic-substring/longest-palindromic-substring.cpp
@@ -1,38 +1,34 @@
 class Solution {
 public:
-    int t[1001][1001];
-    bool solve(int i,int j,string &s)
-    {
-        if(i>=j) return 1;
+    vector<vector<int>> dp;
 
-        if(t[i][j]!=-1) return t[i][j];
+    bool solve(int i, int j, string &s) {
+        if (i >= j) return 1;
+        if (dp[i][j] != -1) return dp[i][j];
 
-        if(s[i] == s[j])
-        {
-            return t[i][j] = solve(i+1, j-1, s);
-        }
-        return t[i][j] = 0;
+        if (s[i] == s[j])
+            return dp[i][j] = solve(i + 1, j - 1, s);
+        return dp[i][j] = 0;
     }
 
-    string longestPalindrome(string s) {
+    string longestPalindrome(string &s) {
         int n = s.length();
-        int maxLen = INT_MIN;
-        int sp = 0;
-        memset(t, -1, sizeof(t));
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i;j<n;j++)
-            {
-                if(solve(i, j, s) == true)
-                {
-                    if(j-i+1> maxLen)
-                    {
-                        maxLen = j-i+1;
-                        sp = i;
+        int maxLen = 0;
+        int startingPoint = 0;
+
+        dp = vector<vector<int>>(n, vector<int>(n, -1));  //Properly initialize class-level dp
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (solve(i, j, s)) {
+                    if (j - i + 1 > maxLen) {
+                        maxLen = j - i + 1;
+                        startingPoint = i;
                     }
                 }
             }
         }
-        return s.substr(sp, maxLen);
-    } 
+
+        return s.substr(startingPoint, maxLen);
+    }
 };
