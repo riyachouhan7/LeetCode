6a4da5409414c3a7a0be235ba92c01106a39b499
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-26 07:00:53 +0530

    Time: 4 ms (9.81%) | Memory: 11 MB (13.56%) - LeetSync

--- 91-decode-ways/decode-ways.cpp
+++ 91-decode-ways/decode-ways.cpp
@@ -1,30 +1,26 @@
 class Solution {
 public:
-    int numDecodingsTopDown(string s, int index, vector<int>& memo) {
-        if (index == s.length()) {
-            return 1;
-        }
+    int numDecodingsTopDown(string s, int index, vector<int>& dp) {
+        if (index == s.length()) return 1;
+        
+        if (s[index] == '0') return 0;
+        
 
-        if (s[index] == '0') {
-            return 0;
-        }
+        if (dp[index] != -1) return dp[index];
+        
 
-        if (memo[index] != -1) {
-            return memo[index];
-        }
-
-        int ways = numDecodingsTopDown(s, index + 1, memo);
+        int ways = numDecodingsTopDown(s, index + 1, dp);
 
         if (index + 1 < s.length() && (s[index] == '1' || (s[index] == '2' && s[index + 1] <= '6'))) {
-            ways += numDecodingsTopDown(s, index + 2, memo);
+            ways += numDecodingsTopDown(s, index + 2, dp);
         }
 
-        memo[index] = ways;
+        dp[index] = ways;
         return ways;
     }
 
     int numDecodings(string s) {
-        vector<int> memo(s.length(), -1);
-        return numDecodingsTopDown(s, 0, memo);
+        vector<int> dp(s.length(), -1);
+        return numDecodingsTopDown(s, 0, dp);
     }
 };
