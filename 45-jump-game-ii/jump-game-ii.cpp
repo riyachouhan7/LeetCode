3f58ed3ce12100b681d1e18234d8122fc1cbb1cf
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-07-29 15:56:30 +0530

    Time: 0 ms (100.00%) | Memory: 20.4 MB (62.70%) - LeetSync

--- 45-jump-game-ii/jump-game-ii.cpp
+++ 45-jump-game-ii/jump-game-ii.cpp
@@ -1,19 +1,18 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int index, vector<int>& dp) {
-        if (index >= nums.size() - 1) return 0; // Reached or surpassed the end
-        if (dp[index] != -1) return dp[index];  // Already calculated
-
-        int minJumps = nums.size(); // Worst case max n-1 jumps
-
-        for (int i = 1; i <= nums[index]; i++) { // Try all possible jumps
-            minJumps = min(minJumps, 1 + solve(nums, index + i, dp));
-        }
-        return dp[index] = minJumps;
-    }
-
     int jump(vector<int>& nums) {
-        vector<int> dp(nums.size(), -1); // use -1 to indicate "not calculated"
-        return solve(nums, 0, dp);
+        int jumps = 0;
+        int end = 0;
+        int farthest = 0;
+
+        for (int index = 0; index < nums.size() - 1; ++index) {
+            farthest = max(farthest, index + nums[index]);
+
+            if (index == end) {
+                jumps++;
+                end = farthest;
+            }
+        }
+        return jumps;
     }
 };
