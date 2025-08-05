ce7eed02161a07416b0bc35b1b4e4b84468540a0
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-08-05 14:28:45 +0530

    Time: 3 ms (18.51%) | Memory: 26.1 MB (60.21%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,29 +1,36 @@
 class Solution {
 public:
     int trap(vector<int>& arr) {
-        int left = 0, right = arr.size() - 1;
-        int leftMax = INT_MIN, rightMax = INT_MIN;
-        int water = 0;
+        int n = arr.size();
 
-        while (left < right) {
-            if (arr[left] < arr[right]) 
+        int left = 0, right = n-1, leftMax = 0, rightMax = 0, water = 0;
+        while(left < right)
+        {
+            if(arr[left] < arr[right])
             {
-                if (arr[left] >= leftMax)
+                if(arr[left] >= leftMax)
+                {
                     leftMax = arr[left];
+                }
                 else
+                {
                     water += leftMax - arr[left];
+                }
                 left++;
-            } 
-            else 
+            }
+            else
             {
-                if (arr[right] >= rightMax)
+                if(arr[right] >= rightMax)
+                {
                     rightMax = arr[right];
+                }
                 else
+                {
                     water += rightMax - arr[right];
+                }
                 right--;
             }
         }
-
         return water;
     }
 };
