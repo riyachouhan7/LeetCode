359112d29bcd35aaf97d242c1818fedb49757bbc
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-09-22 12:26:25 +0530

    Time: 0 ms (100.00%) | Memory: 14.2 MB (48.88%) - LeetSync

--- 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
+++ 153-find-minimum-in-rotated-sorted-array/find-minimum-in-rotated-sorted-array.cpp
@@ -6,8 +6,8 @@ public:
         while(l < r) {
             mid = l + (r-l)/2;
           
-            if(nums[mid] > nums[r]) { //something wrong on right side. Possibly my answer lies in right side.
-                l = mid+1;            //move right (Discarding mid, because it's greater than nums[r], so it can't be the minimum element)
+            if(nums[mid] > nums[r]) { 
+                l = mid+1;            
             } else {
                 r = mid;
             }
