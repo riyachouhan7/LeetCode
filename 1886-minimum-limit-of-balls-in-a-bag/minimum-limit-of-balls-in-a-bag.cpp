83a7ae8b3479b16d1f369abf5b4c92d2929fe08a
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2025-11-07 11:14:58 +0530

    Time: 26 ms (85.93%) | Memory: 59.9 MB (47.07%) - LeetSync

--- 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
+++ 1886-minimum-limit-of-balls-in-a-bag/minimum-limit-of-balls-in-a-bag.cpp
@@ -1,9 +1,10 @@
 class Solution {
 public:
-    bool isPossible(const vector<int>& nums, int maxOps, int mid) {
-        int totalOps = 0;
-        for (int num : nums) {
-            totalOps += (num - 1) / mid;  // Number of splits needed
+    bool isPossible(vector<int>& nums, int maxOps, int mid) {
+        int totalOps = 0; //to bring each number <= mid
+
+        for(int num : nums) {
+            totalOps += (num - 1)/ mid;
             if (totalOps > maxOps) return false;  // Early stop if already exceeding
         }
         return true;
@@ -11,17 +12,19 @@ public:
 
     int minimumSize(vector<int>& nums, int maxOperations) {
         int l = 1;
-        int r = *max_element(nums.begin(), nums.end());
-        int result = r;
+        int r = *max_element(begin(nums), end(nums));
+        
+        int result = r; //we have to minimize this
 
-        while (l <= r) {
-            int mid = l + (r - l) / 2;
+        //O(n * log(MAX))
+        while(l <= r) {
+            int mid = l + (r-l)/2;
 
-            if (isPossible(nums, maxOperations, mid)) {
-                result = mid;      // Possible → try smaller penalty
-                r = mid - 1;
+            if(isPossible(nums, maxOperations, mid) == true) {
+                result = mid;
+                r = mid-1;
             } else {
-                l = mid + 1;       // Not possible → increase penalty
+                l = mid+1;
             }
         }
 
