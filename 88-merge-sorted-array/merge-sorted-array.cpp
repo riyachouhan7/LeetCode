4a70d7cef2031aab06a77c38d2db4b305512ef3d
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2026-05-27 07:58:05 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (71.06%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.cpp
+++ 88-merge-sorted-array/merge-sorted-array.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
-    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+    void merge(vector<int>& nums1, int m,
+               vector<int>& nums2, int n) {
 
-        // Striver's Optimal Approach-1 ->>
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
 
-        int left = m-1;
-        int right = 0;
+        while (i >= 0 && j >= 0) {
 
-        while(left >= 0 && right < n){
-            if(nums1[left] > nums2[right]){
-                swap(nums1[left], nums2[right]);
-                left--;
-                right++;
+            if (nums1[i] > nums2[j]) {
+                nums1[k--] = nums1[i--];
             }
-            else{
-                break;
+            else {
+                nums1[k--] = nums2[j--];
             }
         }
-        sort(nums1.begin(), nums1.begin()+m); // Sorting till m^th index
-        sort(nums2.begin(), nums2.end());
 
-        for(int i=m; i<m+n; i++){
-            nums1[i] = nums2[i-m];
+        while (j >= 0) {
+            nums1[k--] = nums2[j--];
         }
     }
 };
