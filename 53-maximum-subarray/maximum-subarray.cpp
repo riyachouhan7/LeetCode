be21dec8182e2ed0404c123aa8d5e5846599922f
Kumari Riya <75584824+riyachouhan7@users.noreply.github.com>
2026-09-03 21:04:06 +0530

    Time: 0 ms (100.00%) | Memory: 71.6 MB (81.20%) - LeetSync

--- 53-maximum-subarray/maximum-subarray.cpp
+++ 53-maximum-subarray/maximum-subarray.cpp
@@ -5,15 +5,13 @@ public:
         int ans = 0;
         int sum = INT_MIN;
 
-        for(int i = 0; i < n; i++) {
-            ans += nums[i];
+        for(int i=0;i<n;i++) {
+            ans+=nums[i];
             sum = max(sum, ans);
-
             if(ans < 0) {
                 ans = 0;
             }
         }
-
         return sum;
     }
 };
